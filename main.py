cd0c1ef595fa10cb77d307b1d4845cf7bd823802
punkestu <39290636+punkestu@users.noreply.github.com>
2020-10-04 11:54:20 +0700

    Update main.py
    processing the calculation before print it out and make an exception handling

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ v1 = float(input("first Value: "))
 option = str(input("operation: "))
 v2 = float(input("second Value: "))
 
-if option == "+":
-	print("answer:",v1+v2)
-if option == "-":
-	print("answer:",v1-v2)
-if option == "*":
-	print("answer:",v1*v2)
-if option == "/":
-	print("answer:",v1/v2)
+elif option == "+":
+	answer = v1+v2
+	if answer-int(answer) == 0:
+		answer = int(answer)
+elif option == "-":
+	answer = v1-v2
+	if answer-int(answer) == 0:
+		answer = int(answer)
+elif option == "*":
+	answer = v1*v2
+	if answer-int(answer) == 0:
+		answer = int(answer)
+elif option == "/":
+	answer = v1/v2
+	if answer-int(answer) == 0:
+		answer = int(answer)
+else:
+	answer = "the operation is just [+,-,*,/]"
+
+print("answer:",answer)
